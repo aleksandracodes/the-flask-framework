fe8e25335078e27292a7df5fb7f46aa1db6fc8d5
Aleksandra <a.haniok@gmail.com>
2022-03-07 13:33:04 +0000

    Render index.html to the screen

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template # import Flask class
+
+
+app = Flask(__name__)  # create an instance and store in a variable 'app'
+
+
+@app.route("/")  # decorator (a way of wrapping functions)
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "192.168.1.6"),
+        port=int(os.environ.get("PORT", "8080")),
+        debug=True)
