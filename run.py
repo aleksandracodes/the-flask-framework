c436125c304e235e6ab9718d4c3b50ccfaa80737
Aleksandra <a.haniok@gmail.com>
2022-03-07 13:40:11 +0000

    Renender about.html page

--- run.py
+++ run.py
@@ -9,6 +9,12 @@ app = Flask(__name__)  # create an instance and store in a variable 'app'
 def index():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "192.168.1.6"),
