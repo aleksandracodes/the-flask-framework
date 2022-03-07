c0113794da2645a0b6f1ee5f58cb9420e5588682
Aleksandra <a.haniok@gmail.com>
2022-03-07 14:23:31 +0000

    Add template inheritance &routing between webpages

--- run.py
+++ run.py
@@ -15,8 +15,18 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "192.168.1.6"),
-        port=int(os.environ.get("PORT", "8080")),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
